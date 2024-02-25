488fd5a7384cd2c475b1f150743fecc9c25ce305
Przemysław Ren <przemyslaw.ren@gmail.com>
2024-02-26 00:02:45 +0100

    add CrossOrigin

--- frontend/src/exerciseBox/ExerciseBox.js
+++ frontend/src/exerciseBox/ExerciseBox.js
@@ -7,16 +7,16 @@ function ExerciseBox({ exercise }) {
   const prefix =
     'https://github.com/yuhonas/free-exercise-db/blob/main/exercises/';
 
-    const handleOnClick = () => {
-      navigate(`/exercises/${exercise.id}`); // Use the exercise ID to navigate
-    };
+  const handleOnClick = () => {
+    navigate(`/exercises/${exercise.id}`); // Use the exercise ID to navigate
+  };
 
   return (
     <div className="exercise-container" onClick={handleOnClick}>
       <div className="exercise-name">
         <h3>{exercise.name}</h3>
       </div>
-      <div className="exercise-image" >
+      <div className="exercise-image">
         {exercise.images && exercise.images[0] && (
           <img
             src={prefix + exercise.images[0] + '?raw=true'}
