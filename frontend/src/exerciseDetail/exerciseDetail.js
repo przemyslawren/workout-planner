688e32c294047fd2984fb472ca0412b784b181bb
Przemysław Ren <przemyslaw.ren@gmail.com>
2024-02-26 16:59:11 +0100

    fix table with exercise details

--- frontend/src/exerciseDetail/exerciseDetail.js
+++ frontend/src/exerciseDetail/exerciseDetail.js
@@ -4,20 +4,26 @@ import './ExerciseDetail.css';
 
 function ExerciseDetail() {
   const { id } = useParams();
+  console.log('Exercise ID:', id);
   const [exercise, setExercise] = useState(null);
   const prefix =
     'https://github.com/yuhonas/free-exercise-db/blob/main/exercises/';
 
   useEffect(() => {
+    console.log('Fetching exercise for ID:', id);
     fetch(`http://localhost:8080/api/v1/exercises/${id}`)
       .then((response) => response.json())
-      .then((data) => setExercise(data))
+      .then((data) => {
+        console.log('Exercise data:', data);
+        setExercise(data)})
       .catch((error) => console.error('Error fetching exercise:', error));
   }, [id]);
 
   if (!exercise) {
     return <div>Loading...</div>;
   }
+
+  console.log('Rendering exercise:', exercise);
 
   return (
     <div className="exercise-detail-container">
@@ -37,40 +43,59 @@ function ExerciseDetail() {
             <strong>Name</strong>
           </span>
           <span>{exercise.name}</span>
-          <span>
-            <strong>Force</strong>
-          </span>
-          <span>{exercise.force}</span>
+          {exercise?.force && (
+            <>
+              <span>
+                <strong>Force</strong>
+              </span>
+              <span>{exercise.force}</span>
+            </>
+          )}
           <span>
             <strong>Level</strong>
           </span>
           <span>{exercise.level}</span>
-          <span>
-            <strong>Mechanic</strong>
-          </span>
-          <span>{exercise.mechanic}</span>
+
           <span>
             <strong>Category</strong>
           </span>
           <span>{exercise.category}</span>
-          <span>
-            <strong>Equipment</strong>
-          </span>
-          <span>{exercise.equipment}</span>
+          {exercise?.equipment && (
+            <>
+              <span>
+                <strong>Equipment</strong>
+              </span>
+              <span>{exercise.equipment}</span>
+            </>
+          )}
           <span>
             <strong>Primary muscles</strong>
           </span>
-          <span>{exercise.primaryMuscles}</span>
           <span>
-            <strong>Secondary muscles</strong>
+            {exercise.primaryMuscles &&
+              exercise.primaryMuscles.map((primaryMuscles, index) => (
+                <span key={index}>{primaryMuscles + ' '}</span>
+              ))}
           </span>
-          <span>{exercise.secondaryMuscles}</span>
+          {exercise.secondaryMuscles != 0 && (
+            <>
+              <span>
+                <strong>Secondary muscles</strong>
+              </span>
+              <span>
+                {exercise.secondaryMuscles &&
+                  exercise.secondaryMuscles.map((secondaryMuscles, index) => (
+                    <span key={index}>{secondaryMuscles + ' '}</span>
+                  ))}
+              </span>
+            </>
+          )}
         </div>
         <hr className="exercise-detail-info-hr" />
         <div className="exercise-detail-info-instructions">
           {exercise.instructions &&
             exercise.instructions.map((instruction, index) => (
-              <p>{index + 1 + '. ' + instruction + ' '}</p>
+              <p key={index}>{index + 1 + '. ' + instruction + ' '}</p>
             ))}
         </div>
       </div>
